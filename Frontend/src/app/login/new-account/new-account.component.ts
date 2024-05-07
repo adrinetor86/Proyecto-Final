488b570577e0c7a54986586ae93512cc80b8f77b
Jorge H <jorgejoxhdez@gmail.com>
2024-05-08 01:10:13 +0200

    Insercion nuevos usuarios conexion BD

--- Frontend/src/app/login/new-account/new-account.component.ts
+++ Frontend/src/app/login/new-account/new-account.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
-import {ValidService} from "../../servicios/validate.service";
+import { ValidService } from "../../servicios/validate.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-account',
@@ -9,14 +10,15 @@ import {ValidService} from "../../servicios/validate.service";
 })
 export class NewAccountComponent {
   @ViewChild('formNewAccount', { static: false }) formNewAccount: NgForm;
-  constructor(private validService: ValidService) {
+  constructor(private validService: ValidService, private router: Router){
   }
   registerNewUser(): void {
     const email = this.formNewAccount.value.emailNew;
     const username = this.formNewAccount.value.usernameNew;
     const password = this.formNewAccount.value.passwordNew;
-    this.validService.registerNewUser(email, username, password).subscribe(resData =>{
-      console.log(resData);
+    this.validService.registerNewUser(email, username, password).subscribe(() =>{
+      //falta mostrar algun texto de error para el usuario
+      this.router.navigate(['/']);
     });
   }
 }
