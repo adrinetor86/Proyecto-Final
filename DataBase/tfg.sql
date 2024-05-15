491ebe8f5da4de6a491d43308696825bd10937d9
Nahuel <Nahuelhidalgo1204@gmail.com>
2024-05-15 19:24:04 +0200

    instrucciones y nueva base de datos con evento

--- DataBase/tfg.sql
+++ DataBase/tfg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2024 a las 07:07:33
+-- Tiempo de generación: 15-05-2024 a las 19:21:09
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -26,28 +26,32 @@ USE `tfg`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `codes_confirm`
+--
+
+DROP TABLE IF EXISTS `codes_confirm`;
+CREATE TABLE `codes_confirm` (
+  `email` varchar(100) NOT NULL,
+  `code` int(5) DEFAULT NULL,
+  `date_creation` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `comments`
 --
 
 DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id_comment` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comments` (
+  `id_comment` int(11) NOT NULL,
   `user` varchar(15) DEFAULT NULL,
   `id_game` int(11) DEFAULT NULL,
   `content_comment` text NOT NULL,
   `parent_comment` int(11) DEFAULT NULL,
-  `comment_date` datetime NOT NULL,
-  PRIMARY KEY (`id_comment`),
-  KEY `user` (`user`),
-  KEY `id_game` (`id_game`),
-  KEY `fk_comment` (`parent_comment`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `comment_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `comments`
---
-
-TRUNCATE TABLE `comments`;
 --
 -- Volcado de datos para la tabla `comments`
 --
@@ -65,32 +69,25 @@ INSERT INTO `comments` (`id_comment`, `user`, `id_game`, `content_comment`, `par
 --
 
 DROP TABLE IF EXISTS `games`;
-CREATE TABLE IF NOT EXISTS `games` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
   `title` varchar(200) NOT NULL,
   `synopsis` text DEFAULT NULL,
   `developer` varchar(50) NOT NULL,
   `link_download` varchar(200) NOT NULL,
   `link_trailer` varchar(200) DEFAULT NULL,
   `release_date` date DEFAULT NULL,
-  `front_page` longtext DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `title` (`title`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `front_page` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `games`
---
-
-TRUNCATE TABLE `games`;
 --
 -- Volcado de datos para la tabla `games`
 --
 
 INSERT INTO `games` (`id`, `title`, `synopsis`, `developer`, `link_download`, `link_trailer`, `release_date`, `front_page`) VALUES
-(1, 'Genshin Impact', 'Genshin Impact tiene lugar en el mundo de fantasÃ­a de Teyvat, hogar de siete naciones, cada una de las cuales estÃ¡ ligada a un elemento diferente y gobernada por un dios (arconte) diferente. La historia sigue a The Traveller, un aventurero interestelar que, al comienzo del juego, se separa de su hermano gemelo despuÃ©s de que los dos aterrizan en Teyvat. A partir de entonces, el Viajero viaja a travÃ©s de las naciones de Teyvat en busca del hermano perdido, acompaÃ±ado por su guÃ­a, Paimon . En el camino, los dos se hacen amigos de innumerables personas, se involucran en los asuntos de sus naciones y comienzan a desentraÃ±ar los misterios de la tierra.', 'miHoYo', 'https://genshin.hoyoverse.com/es/company/about', 'https://www.youtube.com/watch?v=TAlKhARUcoY&ab_channel=GenshinImpact', '2020-09-28', NULL),
-(2, 'The Forest', 'The Forest es un videojuego de terror y supervivencia desarrollado y publicado por Endnight Games. El juego se desarrolla en una remota penÃ­nsula densamente boscosa, donde el personaje del jugador Eric LeBlanc debe luchar contra monstruos canÃ­bales, mientras busca a su hijo Timmy despuÃ©s de un accidente aÃ©reo.', 'Endnight Games Ltd', 'https://store.steampowered.com/agecheck/app/242760/?l=spanish', 'https://www.youtube.com/watch?v=7mwn5U2PNvk&ab_channel=GameSpot', '2014-05-30', NULL),
-(3, 'Subnautica', 'Desciende a las profundidades de un mundo submarino alienÃ­gena lleno de belleza y peligros. Crea equipamiento, pilota submarinos, terraforma el terreno, y burla los peligros para explorar exhuberantes arrecifes de coral, volcanes, sistemas de cuevas y mÃ¡s - Todo mientras intentas sobrevivir.', 'Unknown Worlds Entertainment', 'https://store.steampowered.com/app/264710/Subnautica/?l=spanish', 'https://www.youtube.com/watch?v=_7BzngvURrk&ab_channel=Subnautica', '2018-01-23', NULL);
+(1, 'Genshin Impact', 'Genshin Impact tiene lugar en el mundo de fantasÃƒÂ­a de Teyvat, hogar de siete naciones, cada una de las cuales estÃƒÂ¡ ligada a un elemento diferente y gobernada por un dios (arconte) diferente. La historia sigue a The Traveller, un aventurero interestelar que, al comienzo del juego, se separa de su hermano gemelo despuÃƒÂ©s de que los dos aterrizan en Teyvat. A partir de entonces, el Viajero viaja a travÃƒÂ©s de las naciones de Teyvat en busca del hermano perdido, acompaÃƒÂ±ado por su guÃƒÂ­a, Paimon . En el camino, los dos se hacen amigos de innumerables personas, se involucran en los asuntos de sus naciones y comienzan a desentraÃƒÂ±ar los misterios de la tierra.', 'miHoYo', 'https://genshin.hoyoverse.com/es/company/about', 'https://www.youtube.com/watch?v=TAlKhARUcoY&ab_channel=GenshinImpact', '2020-09-28', NULL),
+(2, 'The Forest', 'The Forest es un videojuego de terror y supervivencia desarrollado y publicado por Endnight Games. El juego se desarrolla en una remota penÃƒÂ­nsula densamente boscosa, donde el personaje del jugador Eric LeBlanc debe luchar contra monstruos canÃƒÂ­bales, mientras busca a su hijo Timmy despuÃƒÂ©s de un accidente aÃƒÂ©reo.', 'Endnight Games Ltd', 'https://store.steampowered.com/agecheck/app/242760/?l=spanish', 'https://www.youtube.com/watch?v=7mwn5U2PNvk&ab_channel=GameSpot', '2014-05-30', NULL),
+(3, 'Subnautica', 'Desciende a las profundidades de un mundo submarino alienÃƒÂ­gena lleno de belleza y peligros. Crea equipamiento, pilota submarinos, terraforma el terreno, y burla los peligros para explorar exhuberantes arrecifes de coral, volcanes, sistemas de cuevas y mÃƒÂ¡s - Todo mientras intentas sobrevivir.', 'Unknown Worlds Entertainment', 'https://store.steampowered.com/app/264710/Subnautica/?l=spanish', 'https://www.youtube.com/watch?v=_7BzngvURrk&ab_channel=Subnautica', '2018-01-23', NULL);
 
 -- --------------------------------------------------------
 
@@ -99,18 +96,11 @@ INSERT INTO `games` (`id`, `title`, `synopsis`, `developer`, `link_download`, `l
 --
 
 DROP TABLE IF EXISTS `games_genders`;
-CREATE TABLE IF NOT EXISTS `games_genders` (
+CREATE TABLE `games_genders` (
   `id_gender` int(11) NOT NULL,
-  `id_game` int(11) NOT NULL,
-  PRIMARY KEY (`id_gender`,`id_game`),
-  KEY `id_game` (`id_game`)
+  `id_game` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `games_genders`
---
-
-TRUNCATE TABLE `games_genders`;
 --
 -- Volcado de datos para la tabla `games_genders`
 --
@@ -128,19 +118,12 @@ INSERT INTO `games_genders` (`id_gender`, `id_game`) VALUES
 --
 
 DROP TABLE IF EXISTS `games_maps`;
-CREATE TABLE IF NOT EXISTS `games_maps` (
-  `id_map` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `games_maps` (
+  `id_map` int(11) NOT NULL,
   `id_game` int(11) DEFAULT NULL,
-  `url_map` longtext NOT NULL,
-  PRIMARY KEY (`id_map`),
-  KEY `id_game` (`id_game`)
+  `url_map` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `games_maps`
---
-
-TRUNCATE TABLE `games_maps`;
 -- --------------------------------------------------------
 
 --
@@ -148,18 +131,11 @@ TRUNCATE TABLE `games_maps`;
 --
 
 DROP TABLE IF EXISTS `games_plataforms`;
-CREATE TABLE IF NOT EXISTS `games_plataforms` (
+CREATE TABLE `games_plataforms` (
   `id_plataform` int(11) NOT NULL,
-  `id_game` int(200) NOT NULL,
-  PRIMARY KEY (`id_plataform`,`id_game`),
-  KEY `id_game` (`id_game`)
+  `id_game` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `games_plataforms`
---
-
-TRUNCATE TABLE `games_plataforms`;
 --
 -- Volcado de datos para la tabla `games_plataforms`
 --
@@ -176,20 +152,13 @@ INSERT INTO `games_plataforms` (`id_plataform`, `id_game`) VALUES
 --
 
 DROP TABLE IF EXISTS `log_actions`;
-CREATE TABLE IF NOT EXISTS `log_actions` (
+CREATE TABLE `log_actions` (
   `id_log_action` int(11) NOT NULL,
   `user` varchar(15) DEFAULT NULL,
   `game` varchar(100) NOT NULL,
-  `date_creation` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_log_action`),
-  UNIQUE KEY `user` (`user`)
+  `date_creation` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `log_actions`
---
-
-TRUNCATE TABLE `log_actions`;
 --
 -- Volcado de datos para la tabla `log_actions`
 --
@@ -204,18 +173,11 @@ INSERT INTO `log_actions` (`id_log_action`, `user`, `game`, `date_creation`) VAL
 --
 
 DROP TABLE IF EXISTS `rols`;
-CREATE TABLE IF NOT EXISTS `rols` (
-  `id_rol` int(11) NOT NULL AUTO_INCREMENT,
-  `type_rol` varchar(15) NOT NULL,
-  PRIMARY KEY (`id_rol`),
-  UNIQUE KEY `type_rol` (`type_rol`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `rols` (
+  `id_rol` int(11) NOT NULL,
+  `type_rol` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `rols`
---
-
-TRUNCATE TABLE `rols`;
 --
 -- Volcado de datos para la tabla `rols`
 --
@@ -232,18 +194,11 @@ INSERT INTO `rols` (`id_rol`, `type_rol`) VALUES
 --
 
 DROP TABLE IF EXISTS `type_actions`;
-CREATE TABLE IF NOT EXISTS `type_actions` (
-  `id_type_actions` int(11) NOT NULL AUTO_INCREMENT,
-  `type_action` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_type_actions`),
-  UNIQUE KEY `type_action` (`type_action`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `type_actions` (
+  `id_type_actions` int(11) NOT NULL,
+  `type_action` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `type_actions`
---
-
-TRUNCATE TABLE `type_actions`;
 --
 -- Volcado de datos para la tabla `type_actions`
 --
@@ -260,17 +215,11 @@ INSERT INTO `type_actions` (`id_type_actions`, `type_action`) VALUES
 --
 
 DROP TABLE IF EXISTS `type_genders`;
-CREATE TABLE IF NOT EXISTS `type_genders` (
-  `id_type_gender` int(2) NOT NULL AUTO_INCREMENT,
-  `name_gender` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_type_gender`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `type_genders` (
+  `id_type_gender` int(2) NOT NULL,
+  `name_gender` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `type_genders`
---
-
-TRUNCATE TABLE `type_genders`;
 --
 -- Volcado de datos para la tabla `type_genders`
 --
@@ -299,17 +248,11 @@ INSERT INTO `type_genders` (`id_type_gender`, `name_gender`) VALUES
 --
 
 DROP TABLE IF EXISTS `type_plataform`;
-CREATE TABLE IF NOT EXISTS `type_plataform` (
-  `id_type_plataform` int(2) NOT NULL AUTO_INCREMENT,
-  `name_plataform` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_type_plataform`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `type_plataform` (
+  `id_type_plataform` int(2) NOT NULL,
+  `name_plataform` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `type_plataform`
---
-
-TRUNCATE TABLE `type_plataform`;
 --
 -- Volcado de datos para la tabla `type_plataform`
 --
@@ -327,22 +270,14 @@ INSERT INTO `type_plataform` (`id_type_plataform`, `name_plataform`) VALUES
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `username` varchar(15) DEFAULT NULL,
   `password` text NOT NULL,
   `date_creation` date DEFAULT NULL,
-  `rol_user` int(11) DEFAULT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `username` (`username`),
-  KEY `rol_user` (`rol_user`)
+  `rol_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Truncar tablas antes de insertar `users`
---
-
-TRUNCATE TABLE `users`;
 --
 -- Volcado de datos para la tabla `users`
 --
@@ -351,6 +286,140 @@ INSERT INTO `users` (`email`, `username`, `password`, `date_creation`, `rol_user
 ('JacketKawaii@gmail.com', 'Jack_uwu', 'Jacket1234', '2024-05-12', 2),
 ('Nahuelhidalgo1204@gmail.com', 'Darling1204', 'Nahuel123456', '2024-04-20', 1),
 ('usuariorandom@gmail.com', 'random1234', 'random1234', '2024-04-20', 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `codes_confirm`
+--
+ALTER TABLE `codes_confirm`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indices de la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id_comment`),
+  ADD KEY `user` (`user`),
+  ADD KEY `id_game` (`id_game`),
+  ADD KEY `fk_comment` (`parent_comment`);
+
+--
+-- Indices de la tabla `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- Indices de la tabla `games_genders`
+--
+ALTER TABLE `games_genders`
+  ADD PRIMARY KEY (`id_gender`,`id_game`),
+  ADD KEY `id_game` (`id_game`);
+
+--
+-- Indices de la tabla `games_maps`
+--
+ALTER TABLE `games_maps`
+  ADD PRIMARY KEY (`id_map`),
+  ADD KEY `id_game` (`id_game`);
+
+--
+-- Indices de la tabla `games_plataforms`
+--
+ALTER TABLE `games_plataforms`
+  ADD PRIMARY KEY (`id_plataform`,`id_game`),
+  ADD KEY `id_game` (`id_game`);
+
+--
+-- Indices de la tabla `log_actions`
+--
+ALTER TABLE `log_actions`
+  ADD PRIMARY KEY (`id_log_action`),
+  ADD UNIQUE KEY `user` (`user`);
+
+--
+-- Indices de la tabla `rols`
+--
+ALTER TABLE `rols`
+  ADD PRIMARY KEY (`id_rol`),
+  ADD UNIQUE KEY `type_rol` (`type_rol`);
+
+--
+-- Indices de la tabla `type_actions`
+--
+ALTER TABLE `type_actions`
+  ADD PRIMARY KEY (`id_type_actions`),
+  ADD UNIQUE KEY `type_action` (`type_action`);
+
+--
+-- Indices de la tabla `type_genders`
+--
+ALTER TABLE `type_genders`
+  ADD PRIMARY KEY (`id_type_gender`);
+
+--
+-- Indices de la tabla `type_plataform`
+--
+ALTER TABLE `type_plataform`
+  ADD PRIMARY KEY (`id_type_plataform`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `rol_user` (`rol_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `games_maps`
+--
+ALTER TABLE `games_maps`
+  MODIFY `id_map` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rols`
+--
+ALTER TABLE `rols`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `type_actions`
+--
+ALTER TABLE `type_actions`
+  MODIFY `id_type_actions` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `type_genders`
+--
+ALTER TABLE `type_genders`
+  MODIFY `id_type_gender` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `type_plataform`
+--
+ALTER TABLE `type_plataform`
+  MODIFY `id_type_plataform` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -396,6 +465,16 @@ ALTER TABLE `log_actions`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rol_user`) REFERENCES `rols` (`id_rol`);
+
+DELIMITER $$
+--
+-- Eventos
+--
+SET GLOBAL event_scheduler = ON;
+DROP EVENT IF EXISTS `delete_registers`$$
+CREATE DEFINER=`root`@`localhost` EVENT `delete_registers` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-05-15 17:59:15' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM codes_confirm WHERE TIMESTAMPDIFF(MINUTE, date_creation, NOW()) > 4$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
