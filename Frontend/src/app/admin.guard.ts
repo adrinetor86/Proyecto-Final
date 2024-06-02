e215604c6bb3e8f8f354776c727a361d01d399a5
Adrinetor86 <adrinetor81@gmail.com>
2024-06-02 19:16:30 +0200

    guard

---
+++ Frontend/src/app/admin.guard.ts
@@ -0,0 +1,27 @@
+import { inject } from "@angular/core"
+import { Subscription } from "rxjs";
+import { Router } from "@angular/router";
+import {ValidService} from "./servicios/validate.service";
+
+export const adminGuard = () => {
+  const router: Router = inject(Router);
+  const validateService: ValidService = inject(ValidService);
+  let loggedInSubscription: Subscription;
+  let isUserLoggedIn: boolean = false;
+  let comprobarAdmin: boolean = false;
+
+  loggedInSubscription = validateService.isLogged.subscribe(
+    (loggedIn: boolean): void => {
+      isUserLoggedIn = loggedIn;
+      if (isUserLoggedIn) {
+        if (validateService.getUserRole() === 1){
+          comprobarAdmin = true;
+        }else{
+          router.navigate(['/']).then(r => { });
+        }
+      }
+    }
+  )
+
+  return comprobarAdmin;
+}
