364ef63a70bb62593136d10a34fb732a7fa663fd
Adrinetor86 <adrinetor81@gmail.com>
2024-06-09 13:15:13 +0200

    estilado formularios

--- Frontend/src/app/login/forgotten-password/forgotten-password.component.ts
+++ Frontend/src/app/login/forgotten-password/forgotten-password.component.ts
@@ -1,20 +1,28 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ValidService} from "../../servicios/validate.service";
 import {Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
 import {catchError, of} from "rxjs";
 import {RecoveryEmailService} from "../../servicios/recoveryEmail.service";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-forgotten-password',
   templateUrl: './forgotten-password.component.html',
   styleUrl: './forgotten-password.component.css'
 })
-export class ForgottenPasswordComponent {
+export class ForgottenPasswordComponent  implements OnInit {
   @ViewChild('formForgotten', { static: false }) formAccount: NgForm;
   errorMessage = '';
   errorValidate = false;
-  constructor(private validateService: ValidService, private route:Router, private emailAccountForgotten: RecoveryEmailService) {}
+  constructor(private validateService: ValidService, private route:Router, private emailAccountForgotten: RecoveryEmailService,private tituloPagina:Title) {}
+
+
+  ngOnInit() {
+    scrollTo(0,0);
+    this.tituloPagina.setTitle('Recuperar contraseña');
+  }
+
   recoveryPassword(){
     this.emailAccountForgotten.email = this.formAccount.value.email;
     this.validateService.sendCodeRecoveryPassword(this.emailAccountForgotten.email).pipe(
